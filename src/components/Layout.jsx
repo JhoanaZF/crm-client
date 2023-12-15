dc4e8f71a5517ab648d556a9f5447c5acff4f0dc
Jhoana-ZF <jzamora@analytia.pe>
2023-12-14 23:59:55 -0500

    Modificando Link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation;
+  console.log(location);
+
   return (
     <div className='md:flex md:min-h-screen'>
       <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
@@ -9,7 +12,9 @@ const Layout = () => {
         </h2>
         <nav className='mt-10'>
           <Link
-            className='text-2xl block mt-2 hover:text-blue-300 text-white'
+            className={`${
+              location.pathname === "/" ? " text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300 text-white`}
             to='/clientes/nuevo'>
             Nuevo Cliente
           </Link>
